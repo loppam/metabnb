e15e5a21cd6af179e04a7b2474033cd34843a25a
loppam <lolade132@gmail.com>
2023-03-10 17:36:11 +0100

    cho szn

--- src/assets/navbar.jsx
+++ src/assets/navbar.jsx
@@ -1,6 +1,6 @@
 import { useState, React } from "react";
-import logo1 from "../images/vector.png";
-import logo2 from "../images/vector (1).png";
+import logo1 from "../images/Vector.png";
+import logo2 from "../images/Vector (1).png";
 import Connectwallet from "./connectwallet";
 import { FaBars, FaTimes } from "react-icons/fa";
 import { Link } from "react-router-dom";
@@ -13,7 +13,10 @@ const navbar = () => {
         <img src={logo1} alt="" width={41.99} height={36.37} />
         <img src={logo2} alt="" width={188.1} height={32.19} />
       </div>
-      <ul className={Mobile ? "mobile-link" : "nav-links"} onClick={() => setMobile(false)}>
+      <ul
+        className={Mobile ? "mobile-link" : "nav-links"}
+        onClick={() => setMobile(false)}
+      >
         <li>
           <Link to="/">Home</Link>
         </li>
@@ -27,7 +30,7 @@ const navbar = () => {
         <Connectwallet />
       </div>
       <button className="mobile-menu-icon" onClick={() => setMobile(!Mobile)}>
-      {Mobile ? <FaTimes /> : <FaBars />}
+        {Mobile ? <FaTimes /> : <FaBars />}
       </button>
     </nav>
   );
